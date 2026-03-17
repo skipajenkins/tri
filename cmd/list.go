931293b70a93cc1ad5d2b1d9d4b7fe5c3668b082
skipajenkins <skipajenkins@users.noreply.github.com>
2026-03-17 03:16:21 +0000

    Added --all flag

--- cmd/list.go
+++ cmd/list.go
@@ -14,7 +14,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var doneOpt bool
+var (
+	doneOpt bool
+	allOpt  bool
+)
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -28,6 +31,8 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 
 	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show 'Done' Todos")
+
+	listCmd.Flags().BoolVar(&allOpt, "all", false, "Show all Todos")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
@@ -52,7 +57,7 @@ func listRun(cmd *cobra.Command, args []string) {
 
 	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
 	for _, i := range items {
-		if i.Done == doneOpt {
+		if allOpt || i.Done == doneOpt {
 			fmt.Fprintln(w, i.Label()+"\t"+i.PrettyDone()+"\t"+i.PrettyP()+"\t"+i.Text+"\t")
 		}
 	}
